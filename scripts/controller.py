3d08de02643be38a22f59a7bf26da12215d6265f
mac411 <mcman351@gmail.com>
2021-11-10 04:23:54 -0800

    Finish majority of ROS node functionality

--- scripts/controller.py
+++ scripts/controller.py
@@ -1,29 +1,161 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import rospy
+import math
+import numpy as np
+import random
 from std_msgs.msg import String
+from std_msgs.msg import Int64
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Pose, Twist
 
-def robot_state_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+class robot_controller:
 
-def sensor_data_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    def __init__(self):
+        self.status_sub = rospy.Subscriber('detection_status', Int64, self.robot_status_callback)
+        self.detection_status = None
+        self.location_sub = rospy.Subscriber('object_location', Int64, self.location_callback)
+        self.object_location = None
+        self.orientation_sub = rospy.Subscriber('object_orientation', Int64, self.orientation_callback)
+        self.object_orientation = None
+        self.motion_pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size=10)
+        self.motion_command = ModelState('follower_robot',None,None,'base')
+        self.ang_vel = None
+        self.lin_vel = None
+        self.prev_pos = [0, 0, 0]
+        self.next_pos = [0, 0, 0]
 
-def controller():
-    motion_pub = rospy.Publisher('motion_command', String, queue_size=10)
-    state_pub = rospy.Publisher('state_command', String, queue_size=10)
+    def robot_status_callback(self,data):
+        self.detection_status = data.data
+
+    def location_callback(self,data):
+        self.object_location = data.data
+
+    def orientation_callback(self,data):
+        self.object_orientation = data.data
+
+    def update_lin_vel(self):
+        max_distance = 20
+        c1 = 2
+        c2 = 15
+        lin_max = 2
+        #x_index = np.arange(0, max_distance, 1)
+        y1 = np.zeros(c1)
+        y2 = np.linspace(0, lin_max, c2-c1)
+        y3 = lin_max*np.ones(max_distance-c2)
+        y = np.append(y1,y2)
+        y = np.append(y,y3)
+        self.lin_vel = y[int(self.object_location)]
+
+    def update_ang_vel(self):
+        # curve to get angular_velocity
+        c1 = 160
+        c2 = 480
+        # angular velocity = rate of change of the angle dw/dt
+        ang_max = math.pi/4
+        ang_min = -math.pi/4
+        y1 = ang_min*np.ones(c1)
+        y2 = np.linspace(ang_min, ang_max, c2-c1)
+        y3 = ang_max*np.ones(640-c2)
+        y = np.append(y1,y2)
+        ang_indx = np.append(y,y3)
+        self.ang_vel = ang_indx[int(self.object_orientation)]
+
+    def velocity_model(self,n):
+        a = [0.0001, 0.0001, 0.01, 0.0001, 0.0001, 0.0001]
+        dt = 1 #how long we want the loop to take
+        x_prev = self.prev_pos[0]
+        y_prev = self.prev_pos[1]
+        theta_prev = self.prev_pos[2]
+        v = self.lin_vel
+        w = self.ang_vel
+        if w == 0:
+            w = 0.000001
+        err1 = np.random.randn(1,n)*(a[0]*v**2+a[1]*w**2)
+        err2 = np.random.randn(1,n)*(a[2]*v**2+a[3]*w**2)
+        err3 = np.random.randn(1,n)*(a[4]*v**2+a[5]*w**2)
+        vh = v + err1
+        wh = w + err2
+        rh = vh/wh
+        gamma = err3
+        x_next = x_prev - rh*math.sin(theta_prev) + rh*np.sin(theta_prev+wh*dt)
+        y_next = y_prev + rh*math.cos(theta_prev) - rh*np.cos(theta_prev+wh*dt)
+        theta_next = theta_prev + wh*dt + gamma*dt
+        return x_next, y_next, theta_next
+
+    def landmark_model(self, current_pos, obj_pos, sensor_data):
+        #current_pos & obj_pos =[x,y,th]; sensot_data = [distance, bearing]
+        r = math.sqrt((obj_pos[0]-current_pos[0])**2 + (obj_pos[1]-current_pos[1])**2)
+        phi = math.atan2(obj_pos[1]-current_pos[1],obj_pos[0]-current_pos[0])
+        m1 = sensor_data[1]-phi
+        m2 = abs(m1+2*math.pi)
+        m3 = abs(m1-2*math.pi)
+        mean_phi = min(abs(m1), m2, m3)
+        q = self.normpdf(sensor_data[0]-r, 0, 0.1) * self.normpdf(mean_phi, 0, 0.09) #zero mean gausian distribution
+        return q
+
+    def normpdf(self, x, mean, sd):
+        var = float(sd)**2
+        denom = (2*math.pi*var)**.5
+        num = math.exp(-(float(x)-float(mean))**2/(2*var))
+        return num/denom
+
+    def motion_model(self, obj_pos, sensor_data):
+        #just spin if not locked on
+        if self.detection_status == 0:
+            self.lin_vel = 0
+            self.ang_vel = 0.1*math.pi
+            x, y, t = self.velocity_model(1)
+            self.next_pos = [x, y, t]
+        else:
+            n = 1000
+            #obj_pos = [2, 0]
+            #sensor_data = [1, 0]
+            # prev_pos from most_likely_next_pos
+            # lin_vel from ultrasound sensor_data
+            #angular_velocity from sensor_data
+            x, y, t = self.velocity_model(n)
+            q = []
+            for i in range(0,n):
+                next = [x[0,i], y[0,i], t[0,i]]
+                # object_pos from gazebo ("human") x,y
+                # sensor_data = ultrasound data
+                q_new = self.landmark(next, obj_pos, sensor_data)
+                q.append(q_new)
+            idx = range(0,n)
+            k = 100
+            rwi = random.choices(idx,weights = q, k = k)
+            sx = 0
+            sy = 0
+            st = 0
+            for i in range(0,k):
+                samp_pos = x[0,rwi[i]], y[0,rwi[i]], t[0,rwi[i]]
+                sx = samp_pos[0]+sx
+                sy = samp_pos[1]+sy
+                st = samp_pos[2]+st
+            x_a = sx/k
+            y_a = sy/k
+            t_a = st/k
+            self.prev_pos = self.next_pos
+            self.next_pos = [x_a, y_a, t_a]
+            # print('next_pos', next_pos) #position for simulator and prev_position
+
+    def publish_command(self):
+        self.update_ang_vel()
+        self.update_lin_vel()
+        self.motion_command.twist.linear.x = self.lin_vel
+        self.motion_command.twist.angular.z = self.ang_vel
+        self.motion_pub.publish(self.motion_command)
+
+def control():
     rospy.init_node('controller', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rc = robot_controller()
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        message = 'Test' 
-        rospy.loginfo(message)
-        motion_pub.publish(message)
-        state_pub.publish(message)
-        rospy.Subscriber('robot_state', String, robot_state_callback)
-        rospy.Subscriber('sensor_data', String, sensor_data_callback)
+        rc.publish_command()
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        controller()
+        control()
     except rospy.ROSInterruptException:
         pass
--- scripts/image_processor.py
+++ scripts/image_processor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import rospy
 import cv2
 import numpy as np
@@ -8,132 +8,119 @@ import matplotlib.pyplot as plt
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import uint8
+from std_msgs.msg import Int64
 
-def smooth(y, box_pts):
-    box = np.ones(box_pts)/box_pts
-    y_smooth = np.convolve(y, box, mode='same')
-    return y_smooth
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# def smooth(y, box_pts):
+#     box = np.ones(box_pts)/box_pts
+#     y_smooth = np.convolve(y, box, mode='same')
+#     return y_smooth
 
-#cv2.startWindowThread()
-
-# open webcam video stream
-#cap = cv2.VideoCapture(0)
-cap  = None                          #STREAM OF FRAMES
+# cv2.startWindowThread()
 # the output will be written to output.avi
-out = cv2.VideoWriter(
-    'output.avi',
-    cv2.VideoWriter_fourcc(*'MJPG'),
-    15.,
-    (640,480))
+# out = cv2.VideoWriter(
+#     'output.avi',
+#     cv2.VideoWriter_fourcc(*'MJPG'),
+#     15.,
+#     (640, 480))
 
-center_pixel = 320
-current_state = 0
+# # When everything done, release the capture
+# cap.release()
+# # and release the output
+# out.release()
+# # finally, close the window
+# cv2.destroyAllWindows()
+# cv2.waitKey(1)
 
-def processor_callback(data):
-    # Capture frame-by-frame
-    cap = data
-    ret, frame = cap.read()
+class image_processor:
 
-    # resizing for faster detection
-    frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
-    biggest_box_index = 0
-    print('all boxes', boxes)
-    print('length', len(boxes))
-    size = []
-    weight = []
-    for (x, y, w, h) in boxes:
-        size.append(w*h)
-    #biggest_box_index = size.index(max(size))
-    total_area = sum(size)
-    weight = [i / total_area for i in size]
-    #print('weight',weight)
-
-    avg_center = 0
-    xweight = 0
-    i = 0
-    for (xA, yA, w, h) in boxes:
-        xB = xA+w
-        yB = yA+h
-        # display the detected boxes in the colour picture
-        #cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          #(255, 0, 0), 2)
-        xD = xB-xA
-        xC = int(xA+xD/2)
-        #if xC > 0:
-            #cv2.line(frame, (xC,0), (xC,480), (0,255,0), 4)
-        xweight = xweight + weight[i]*xC
-        i = i+1
-    #print('weghted center', int(xweight))
-    if len(boxes) > 0 :
-        current_state = 1 #locked
-        center_pixel = int(xweight)
-        #cv2.line(frame, (int(xweight),0), (int(xweight),480), (0,0,255), 4)
-        $angular_velocity_not_smooth = ang_indx[int(xweight)]
-    else:
-        current_state = 0 #idle
-
-    #avt = np.append(avt, angular_velocity_not_smooth)
-    #avs = smooth(avt, 25)
-    #angular_velocity = 2*avs[len(avs)-1]
-    #avts = np.append(avts, angular_velocity)
-
-    #print('ang_vel', angular_velocity)
-    #print('ang_vel_smooth', avts)
-    #cv2.line(frame, (640,0), (640,480), (0,255,0),6)
-
-    # Write the output video
-    # out.write(frame.astype('uint8'))
-    # Display the resulting frame
-    # cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    state_pub.publish(np.uint8(current_state)
-    pixel_pub.publish(np.uint8(center_pixel))
-# When everything done, release the capture
-cap.release()
-# and release the output
-out.release()
-# finally, close the window
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+    def __init__(self):
+        self.image_sub = rospy.Subscriber('follower_robot/camera1/image_topic', Image, self.processor_callback)
+        self.status_pub = rospy.Publisher('detection_status', Int64, queue_size=10)
+        self.orientation_pub = rospy.Publisher('object_orientation', Int64, queue_size=10)
+        self.bridge = CvBridge()
+        # initialize the HOG descriptor/person detector
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        self.detection_status = Int64()
+        self.object_orientation = Int64()
 
 
-def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2", Image)
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
-def image_callback(self,data):
-    try:
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-        print(e)
-    cv2.imshow("Image Window", cv_image)
+    def processor_callback(self, data):
+        # Convert image to make it readable by OpenCV
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
-def motion_command_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    motion_command = data
+        # Resizing for faster detection
+        # self.cv_image = cv2.resize(self.cv_image, (640, 480))
 
-def state_command_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    state_command = data
+        # Use grayscale picture, also for faster detection
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+
+        # Detect people in the image
+        # Returns the bounding boxes for the detected objects
+        boxes, weights = self.hog.detectMultiScale(cv_image, winStride=(8, 8))
+        # biggest_box_index = 0
+        # print('all boxes', boxes)
+        # print('length', len(boxes))
+        size = []
+        weight = []
+        for (x, y, w, h) in boxes:
+            size.append(w*h)
+        # biggest_box_index = size.index(max(size))
+        total_area = sum(size)
+        weight = [i / total_area for i in size]
+        # print('weight',weight)
+
+        # avg_center = 0
+        xweight = 0
+        i = 0
+        for (xA, yA, w, h) in boxes:
+            xB = xA+w
+            # yB = yA+h
+            # display the detected boxes in the colour picture
+            # cv2.rectangle(frame, (xA, yA), (xB, yB),
+                            # (255, 0, 0), 2)
+            xD = xB-xA
+            xC = int(xA+xD/2)
+            # if xC > 0:
+                # cv2.line(frame, (xC,0), (xC,480), (0,255,0), 4)
+            xweight = xweight + weight[i]*xC
+            i = i+1
+        # print('weighted center', int(xweight))
+        if len(boxes) > 0:
+            self.detection_status.data = int(1)  # locked
+            self.object_orientation.data = int(xweight)
+            # cv2.line(frame, (int(xweight),0), (int(xweight),480), (0,0,255), 4)
+            # angular_velocity_not_smooth = ang_indx[int(xweight)]
+        else:
+            self.detection_status.data = int(0)  # idle
+
+        # avt = np.append(avt, angular_velocity_not_smooth)
+        # avs = smooth(avt, 25)
+        # angular_velocity = 2*avs[len(avs)-1]
+        # avts = np.append(avts, angular_velocity)
+
+        # print('ang_vel', angular_velocity)
+        # print('ang_vel_smooth', avts)
+        # cv2.line(frame, (640,0), (640,480), (0,255,0),6)
+
+        # Write the output video
+        # out.write(frame.astype('uint8'))
+        # Display the resulting frame
+        # cv2.imshow('frame',frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
 def image_process():
     rospy.init_node('image_processor', anonymous=True)
-    state_pub = rospy.Publisher('state', uint8, queue_size=10)
-    pixel_pub = rospy.Publisher('pixel', uint8, queue_size=10)
-    rospy.Subscriber('follower_robot/camera1/image_topic', Image, processor_callback)
-    rospy.spin()
-
+    ip = image_processor()
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        ip.status_pub.publish(ip.detection_status)
+        ip.orientation_pub.publish(ip.object_orientation)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
